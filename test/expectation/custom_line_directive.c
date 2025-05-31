0a8a3a2de79f5bafd599dbb636227ef4c9844b3e
Konstantin Tomashevich <konstantin.tomashevich@gmail.com>
2025-06-01 01:37:01 +0300

    Add custom line directive test.

---
+++ test/expectation/custom_line_directive.c
@@ -0,0 +1,15 @@
+#line 1 "source/custom_line_directive.c"
+#line 42 "our own rules here"
+#line 1 "include/include_full/trivial.h"
+#line 2 "include/include_full/trivial.h"
+
+int function_1 ();
+int function_2 ();
+
+#line 7 "include/include_full/trivial.h"
+#line 43 "our own rules here"
+
+int main (int argc, char **argv)
+{
+    return function_1 ( ) + function_2 ( ) ;
+}
---
+++ test/source/custom_line_directive.c
@@ -0,0 +1,7 @@
+#line 42 "our own rules here"
+#include <include_full/trivial.h>
+
+int main (int argc, char **argv)
+{
+    return SOME_MACRO;
+}
