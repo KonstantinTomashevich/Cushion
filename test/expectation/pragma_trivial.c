847464d3c36f93dc6c2991afbd4cd116cc0fd558
Konstantin Tomashevich <konstantin.tomashevich@gmail.com>
2025-06-19 16:15:18 +0300

    Change pragma test according to fixes from Kan. It is not as pretty as before, but not it is always guaranteed to put pragma on new line.

--- test/expectation/pragma_trivial.c
+++ test/expectation/pragma_trivial.c
@@ -5,14 +5,26 @@
 
 #line 6 "source/pragma_trivial.c"
 #pragma B
+
+
+#line 7 "source/pragma_trivial.c"
 #pragma C
 
 #line 7 "source/pragma_trivial.c"
 #pragma D
 
 #pragma reflection_array_type interned_string_t
+
+
+#line 10 "source/pragma_trivial.c"
 #pragma reflection_array_type unsigned int
+
+
+#line 11 "source/pragma_trivial.c"
 #pragma reflection_array_type struct my_struct_t
+
+
+#line 12 "source/pragma_trivial.c"
 #pragma reflection_array_type enum my_enum_t
 
 int main (int argc, char **argv)
